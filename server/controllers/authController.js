b4ca856cbea81c6eac8a8628265c57337d1728a4
obicerj <obicerjestoni@gmail.com>
2023-02-24 23:25:30 +0800

    Add controller for login and registration

---
+++ server/controllers/authController.js
@@ -0,0 +1,69 @@
+const { hashPassword, verifyPassword } = require('../utils/password');
+const jwt = require('jsonwebtoken');
+const User = require('../models/user');
+
+async function signup(req, res) {
+  try {
+    const { fullName, email, password } = req.body;
+    const hashedPassword = await hashPassword(password);
+    const alreadyExistsUser = await User.findOne({ where: { email } }).catch(
+      (err) => {
+        console.log('Error: ', err);
+      }
+    );
+
+    if (alreadyExistsUser) {
+      return res.status(409).json({ message: 'User with email already exists!' });
+    }
+
+    const newUser = new User({ fullName, email, password: hashedPassword });
+    const savedUser = await newUser.save().catch((err) => {
+      console.log('Error: ', err);
+      res.status(500).json({ error: 'Cannot register user at the moment!' });
+    });
+
+    if (savedUser) res.json({ message: 'Thanks for registering' });
+  } catch(err) {
+    console.log(err);
+    return res.status(500).json({ message: 'Something went wrong' });
+  }
+}
+
+
+async function login(req, res) {
+  try {
+    const { email, password } = req.body;
+    
+    const user = await User.findOne({ where: { email } }).catch(
+      (err) => {
+        console.log('Error: ', err);
+      }
+    );
+    
+    const isPasswordValid = await verifyPassword(password, user.password);
+
+    if (!user) {
+      return res
+        .status(400)
+        .json({ message: 'Email or password does not match!' });
+    }
+
+    if (!isPasswordValid) throw new Error('Invalid password');
+
+    const jwtToken = jwt.sign(
+      { id: user.id, email: user.email },
+      process.env.JWT_KEY
+    );
+
+    res.json({ message: 'Welcome Back!', token: jwtToken });
+  } catch(err) {
+    console.log(err)
+    return res.status(401).json({ message: 'Invalid credentials' });
+  }
+
+}
+
+module.exports = {
+  signup,
+  login
+}
--- server/routes/login.js
+++ server/routes/login.js
@@ -1,36 +1,36 @@
 const express = require('express');
-const jwt = require('jsonwebtoken');
-const User = require('../models/user');
 
 const router = express.Router();
+const authController = require('../controllers/authController');
 
-router.post('/login', async (req, res) => {
-  const { email, password } = req.body;
+router.post('/login', authController.login);
+// router.post('/login', async (req, res) => {
+//   const { email, password } = req.body;
 
-  const userWithEmail = await User.findOne({ where: { email } }).catch(
-    (err) => {
-      console.log('Error: ', err);
-    }
-  );
+//   const userWithEmail = await User.findOne({ where: { email } }).catch(
+//     (err) => {
+//       console.log('Error: ', err);
+//     }
+//   );
 
-  if (!userWithEmail) {
-    return res
-      .status(400)
-      .json({ message: 'Email or password does not match!' });
-  }
+//   if (!userWithEmail) {
+//     return res
+//       .status(400)
+//       .json({ message: 'Email or password does not match!' });
+//   }
 
-  if (userWithEmail.password !== password) {
-    return res
-      .status(400)
-      .json({ message: 'Email or password does not match!' });
-  }
+//   if (userWithEmail.password !== password) {
+//     return res
+//       .status(400)
+//       .json({ message: 'Email or password does not match!' });
+//   }
 
-  const jwtToken = jwt.sign(
-    { id: userWithEmail.id, email: userWithEmail.email },
-    process.env.JWT_KEY
-  );
+//   const jwtToken = jwt.sign(
+//     { id: userWithEmail.id, email: userWithEmail.email },
+//     process.env.JWT_KEY
+//   );
 
-  res.json({ message: 'Welcome Back!', token: jwtToken });
-});
+//   res.json({ message: 'Welcome Back!', token: jwtToken });
+// });
 
 module.exports = router;
