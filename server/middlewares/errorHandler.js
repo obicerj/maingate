2703fe514b9c3e2c3967100d3705ab2b2ccf3178
obicerj <obicerjestoni@gmail.com>
2023-02-24 23:24:07 +0800

    Fix for docker and env config

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -11,7 +11,7 @@ function errorHandler(err, req, res, next) {
   res.status(statusCode);
   res.json({
     message: err.message,
-    stack: process.env.NODE_ENV === 'production' ? '🥞' : err.stack
+    stack: process.env.APP_ENV === 'production' ? '🥞' : err.stack
   });
 }
 
