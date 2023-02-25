42d5b7cb3afcfb90b51971dc3cd146b31b60690e
obicerj <obicerjestoni@gmail.com>
2023-02-25 16:05:50 +0800

    Add logout

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,10 +1,16 @@
 const express = require('express');
+const passport = require('passport');
 const registerApi = require('./register');
 const loginApi = require('./login');
+
 
 const router = express.Router();
 
 router.use(registerApi);
 router.use(loginApi);
+
+router.post('/logout', passport.authenticate('jwt', { session: false }), (req, res) => {
+  res.json({ message: 'User logged out successfully' });
+});
 
 module.exports = router;
