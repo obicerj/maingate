aa506084e357f564cf320dec1dc5dafa973fafbc
obicerj <obicerjestoni@gmail.com>
2023-02-25 15:54:16 +0800

    Remove commented code

--- server/routes/register.js
+++ server/routes/register.js
@@ -4,26 +4,5 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 
 router.post('/register', authController.signup);
-// router.post('/register', async (req, res) => {
-//   const { fullName, email, password } = req.body;
-
-//   const alreadyExistsUser = await User.findOne({ where: { email } }).catch(
-//     (err) => {
-//       console.log('Error: ', err);
-//     }
-//   );
-
-//   if (alreadyExistsUser) {
-//     return res.status(409).json({ message: 'User with email already exists!' });
-//   }
-
-//   const newUser = new User({ fullName, email, password });
-//   const savedUser = await newUser.save().catch((err) => {
-//     console.log('Error: ', err);
-//     res.status(500).json({ error: 'Cannot register user at the moment!' });
-//   });
-
-//   if (savedUser) res.json({ message: 'Thanks for registering' });
-// });
 
 module.exports = router;
