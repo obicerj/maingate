5dc9417549e41aee97568a3fd7db6754653702a4
obicerj <obicerjestoni@gmail.com>
2023-02-25 15:53:36 +0800

    Remove demo

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,12 +1,10 @@
 const express = require('express');
 const registerApi = require('./register');
 const loginApi = require('./login');
-const demoApi = require('./demo');
 
 const router = express.Router();
 
 router.use(registerApi);
 router.use(loginApi);
-router.use(demoApi);
 
 module.exports = router;
